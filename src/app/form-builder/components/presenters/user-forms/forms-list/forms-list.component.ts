3216969059af3a53590f064254ed552e14886689
urbanovy4 <urbanovy4@gmail.com>
2022-01-12 21:48:35 +0200

    delete all files

--- src/app/form-builder/components/presenters/user-forms/forms-list/forms-list.component.ts
+++
@@ -1,9 +0,0 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
-
-@Component({
-  selector: 'app-forms-list',
-  templateUrl: './forms-list.component.html',
-  styleUrls: ['./forms-list.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
-})
-export class FormsListComponent {}
